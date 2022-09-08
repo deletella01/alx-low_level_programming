2408646d99e3686dd9841d4a8093b4f510e0ac34
deletella01 <deletella01@gmail.com>
2022-09-08 22:18:19 +0000

    C programming initial commit task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+* This is a simple program to output a string with
+* the put function.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
