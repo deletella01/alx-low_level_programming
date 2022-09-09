1dd2101c2a74d145fb2c6dc549b9cb94f9709d7e
deletella01 <deletella01@gmail.com>
2022-09-09 14:33:59 +0000

    Initial commit for Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* This program will assign a random number to a variable n each time it is
+* executed and it would print if the number assigned to n
+* is positive or negative.
+* I make use of if statements to check if n is greater than 0 (positive), less
+* than 0 (negative), or equal to 0.
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		/* if n is greater than zero, then n is positive*/
+		printf("%d is positive", n);
+	} else if (n < 0)
+	{
+		/* if n is less than zero, then n is negative*/
+		printf("%d is negative", n);
+	} else
+	{
+		/* if n is neither greater than or equal to zero, then it is zero */
+		printf("%d is zero", n);
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+## This section aims to shed light on some operations in C Programming. 
+We would look at arithmetic operators and their usage, logical operators (Boolean Operators) and their usage, relational operators and their usage, how conditional statements are written, how to put comments in-between lines of code, how to declare variables of types char, int. unsigned int and how to print them with printf.
+
+We take a look at ASCII character set, purpose of the gcc flags -m32 and -m64.
