d8ea451081ffefa192d14bd58f1ab10d25fdcdae
deletella01 <deletella01@gmail.com>
2022-09-09 23:25:16 +0000

    Initial commit for Task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* This program will assign a random number to the variable n and will output
+* if the last digit of n is 5 or greater, is 0, or is less than 6 but
+* not 0.
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	lastdigit = n % 10;
+	if (lastdigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, lastdigit);
+	} else if (lastdigit == 0)
+	{
+		printf("Last digit is %d is %d", n, lastdigit);
+	} else
+	{
+		printf"Last digit of %d is %d and is less than 6 and not 0", n, lastdigit);
+	}
+	printf("\n")
+	return (0);
+}
