cd1a5981ad4a0cd7328027ec4808b372e8603b18
deletella01 <deletella01@gmail.com>
2022-09-10 00:15:10 +0000

    Initial commit for Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* This program prints the alphabet in lowercase exempting 'q' and 'e'.
+*
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		while (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
