191e7d1ff7104eec5389cbf3b2485827053c7d07
deletella01 <deletella01@gmail.com>
2022-09-14 22:41:59 +0000

    Initial commit correction for Task 1 to 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,30 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if ((a > b && b > c) || (a > c && c > b))
-    {
-        largest = a;
-    }
-    else if ((b > a && a > c) || (b > c && c > a))
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b)
+	{
+		if (a > c)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = c;
+		}
+	}
+	else
+	{
+		if (b > c)
+		{
+			largest = b;
+		}
+		else
+		{
+			largest = c;
+		}
+	}
 
-    return (largest);
+	return (largest);
 }
