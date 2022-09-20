7871ff5a0e9f3a99da017e43814892b48ee12bb4
deletella01 <deletella01@gmail.com>
2022-09-20 01:03:44 +0000

    Initial commit for Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,8 @@
 * reset_to_98 - this function takes a pointer and updates the value
 *
 * @n: pointer points to 98
-*
-* Return: 0
 */
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
