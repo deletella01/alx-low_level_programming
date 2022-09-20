249e44e29c058314ff1887d19ef7cb154f68950e
deletella01 <deletella01@gmail.com>
2022-09-20 01:53:55 +0000

    Correction for Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-* _strlen - this function returns the length of a string
-*
-* @s: String value
-
-* Return: length of string
-*/
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 
 int _strlen(char *s)
 {
