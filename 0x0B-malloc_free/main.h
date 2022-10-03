c0bca22d92e93601608547ac83f0b3f79690815c
deletella01 <deletella01@gmail.com>
2022-10-03 18:13:43 +0000

    Initial commit for Task 1 to 6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int size);
-int main(void);
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
